bc7eb3226dbc9af2f3b9d6751b7ecc1998aa8458
Guto Xavier <gutoffline@gmail.com>
2023-06-23 19:27:48 -0300

    mer, der, sql de criação do banco de dados e da tabela livros

---
+++ documentacao/banco de dados.sql
@@ -0,0 +1,66 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Tempo de geração: 24-Jun-2023 às 00:26
+-- Versão do servidor: 10.4.27-MariaDB
+-- versão do PHP: 8.2.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `bd_livraria`
+--
+DROP DATABASE IF EXISTS `bd_livraria`;
+CREATE DATABASE IF NOT EXISTS `bd_livraria` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `bd_livraria`;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `tb_livros`
+--
+
+CREATE TABLE `tb_livros` (
+  `id` int(11) NOT NULL,
+  `isbn` varchar(25) DEFAULT NULL,
+  `titulo` varchar(150) DEFAULT NULL,
+  `ano` int(11) DEFAULT NULL,
+  `valor` float DEFAULT NULL,
+  `editora` varchar(150) DEFAULT NULL,
+  `autor` varchar(150) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Índices para tabelas despejadas
+--
+
+--
+-- Índices para tabela `tb_livros`
+--
+ALTER TABLE `tb_livros`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT de tabelas despejadas
+--
+
+--
+-- AUTO_INCREMENT de tabela `tb_livros`
+--
+ALTER TABLE `tb_livros`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
